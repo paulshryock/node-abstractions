de11019fd31f262bfea465044d70f2e1f6d89dee
Paul Shryock <paul@pshry.com>
2023-11-19 09:01:22 -0500

    Remove CommandLine.getArguments method

--- src/CommandLine/CommandLine.ts
+++ src/CommandLine/CommandLine.ts
@@ -4,24 +4,16 @@ import { FinalClassWasExtended } from '../Exception/Exception.ts'
  * Represents a command line interface for terminal input and output.
  *
  * @throws {FinalClassWasExtended}
+ * @alpha
  */
 export class CommandLine {
 	/**
 	 * Constructs a command line abstraction.
 	 *
-	 * @since unreleased
+	 * @throws {FinalClassWasExtended}
+	 * @since  unreleased
 	 */
 	public constructor() {
 		if (new.target !== CommandLine) throw new FinalClassWasExtended(CommandLine)
-	}
-
-	/**
-	 * Gets command line arguments.
-	 *
-	 * @return {string[]} List of arguments from the command line.
-	 * @since  unreleased
-	 */
-	public getArguments(): string[] {
-		return []
 	}
 }
--- tests/CommandLine/CommandLine.test.ts
+++ tests/CommandLine/CommandLine.test.ts
@@ -8,15 +8,44 @@ describe('CommandLine', () => {
 	})
 
 	it('should not be extensible', () => {
-		/** Example of attempting to extend CommandLine. */
-		class MyCommandLine extends CommandLine {}
+		expect(() => new (class extends CommandLine {})()).toThrow(
+			FinalClassWasExtended,
+		)
+	})
+})
 
-		expect(() => new MyCommandLine()).toThrow(FinalClassWasExtended)
+describe('CommandLine.getPositionalArguments()', () => {
+	it.todo('should get positional arguments')
+})
+
+describe('CommandLine.getOptions(Options)', () => {
+	describe('when default values are not set', () => {
+		it.todo('should only get available options')
 	})
 
-	describe('CommandLine.getArguments()', () => {
-		it('should get an empty array if there are no arguments', () => {
-			expect(new CommandLine().getArguments()).toStrictEqual([])
-		})
+	describe('when default values are set', () => {
+		it.todo('should get available options')
+		it.todo('should get default values for unavailable options')
 	})
+})
+
+describe('CommandLine.ask(Question)', () => {
+	it.todo('should write a question to stdout')
+	it.todo('should return an answer from stdin')
+})
+
+describe('CommandLine.out(message)', () => {
+	it.todo('should write a message to stdout')
+})
+
+describe('CommandLine.out(message, { trace: true })', () => {
+	it.todo('should write a stack trace to stdout')
+})
+
+describe('CommandLine.error(Error)', () => {
+	it.todo('should write an Error message to stderr')
+})
+
+describe('CommandLine.error(Error, { trace: true })', () => {
+	it.todo('should write a stack trace to stderr')
 })

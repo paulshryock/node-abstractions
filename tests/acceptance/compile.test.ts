162afa79985c033e05b3b23f344985ce83c05b50
Paul Shryock <paul@pshry.com>
2024-09-20 20:45:53 -0400

    Refactor some tests

--- tests/acceptance/compile.test.ts
+++ tests/acceptance/compile.test.ts
@@ -2,15 +2,13 @@ import { describe, expect, it } from '@jest/globals'
 import { join } from 'node:path'
 import { stat } from 'node:fs/promises'
 
+const BUNDLE_MAXIMUM_SIZE = 1024 * 3
+
 describe('compiled javascript bundle', () => {
 	const pathToBundle = join(__dirname, '..', '..', 'dist', 'index.js')
 
-	it('should be no more than 3kb', async () => {
-		const { size } = await stat(pathToBundle)
-		const sizeInKb = size / 1024
-
-		expect(sizeInKb).toBeLessThan(3)
-	})
+	it('should be no more than 3 KiB', async () =>
+		expect((await stat(pathToBundle)).size).toBeLessThan(BUNDLE_MAXIMUM_SIZE))
 
 	it('should export a CommandLine class', async () => {
 		const { CommandLine } = await import(pathToBundle)
--- tests/unit/bin/prepare.test.ts
+++ tests/unit/bin/prepare.test.ts
@@ -1,32 +1,45 @@
-import { beforeEach, describe, expect, it, jest } from '@jest/globals'
-import { env } from 'node:process'
+import {
+	afterEach,
+	beforeEach,
+	describe,
+	expect,
+	it,
+	jest,
+} from '@jest/globals'
+import process from 'node:process'
 import { resolve } from 'node:path'
+
+jest.mock('husky', () => mockInstall)
 
 // eslint-disable-next-line no-var -- var is hoisted with jest.mock.
 var mockInstall = jest.fn()
-
-jest.mock('husky', () => mockInstall)
+const envOriginal = process.env
+const filePath = resolve(__dirname, '../../../bin/prepare.ts')
 
 beforeEach(() => {
 	jest.resetModules()
 })
 
-describe('prepare script', () => {
-	const filePath = resolve(__dirname, '../../../bin/prepare.ts')
+afterEach(() => {
+	process.env = envOriginal
+})
 
-	it('should install husky if ci is not defined', async () => {
-		delete env.CI
-
-		await import(filePath)
-
-		expect(mockInstall).toHaveBeenCalledTimes(1)
+describe('when ci environment variable is not set', () => {
+	beforeEach(() => {
+		delete process.env.CI
 	})
 
-	it('should not install husky if ci is defined', async () => {
-		env.CI = 'true'
+	it('should install husky', async () => (
+		await import(filePath), expect(mockInstall).toHaveBeenCalledTimes(1)
+	))
+})
 
-		await import(filePath)
-
-		expect(mockInstall).toHaveBeenCalledTimes(0)
+describe('when ci environment variable is "true"', () => {
+	beforeEach(() => {
+		process.env.CI = 'true'
 	})
+
+	it('should not install husky', async () => (
+		await import(filePath), expect(mockInstall).toHaveBeenCalledTimes(0)
+	))
 })

6a618158ea7bbdd3f605e68c48e26d41a1e6fbdc
Paul Shryock <paul@pshry.com>
2024-09-22 05:29:51 -0400

    Refactor some tests

--- tests/unit/CommandLine/CommandLine.test.ts
+++ tests/unit/CommandLine/CommandLine.test.ts
@@ -1,167 +1,169 @@
 import { afterAll, beforeEach, describe, expect, it, jest } from '@jest/globals'
 import { CommandLine } from '../../../src/CommandLine/CommandLine.ts'
 import { Duplex } from 'node:stream'
+import { FinalClassWasExtended } from '../../../src/Exception/Exception.ts'
 import process from 'node:process'
 import { Streams } from '../../../src/CommandLine/Streams.ts'
 
-describe('CommandLine instance', () => {
-	it('should instantiate', () => {
-		expect(() => new CommandLine()).not.toThrow()
-	})
+it('should instantiate', () => expect(() => new CommandLine()).not.toThrow())
 
-	it('should not be extensible', () => {
-		expect(() => new (class extends CommandLine {})()).toThrow(Error)
-	})
+describe('when attempting to extend this class', () => {
+	it('should throw an exception', () =>
+		expect(() => new (class extends CommandLine {})()).toThrow(
+			FinalClassWasExtended,
+		))
 })
 
-describe('CommandLine instance properties', () => {
-	describe.each([
-		[
-			'when there are no arguments',
-			{ argv: [], options: {}, positionalArguments: [] },
-		],
-		[
-			'when there are positional arguments',
-			{
-				argv: ['hello', 'world'],
-				options: {},
-				positionalArguments: ['hello', 'world'],
-			},
-		],
-		[
-			/** @todo Make this its own test. */
-			'when there are flags',
-			{
-				argv: [
-					// String.
-					'-a',
-					'a',
-					// Compound booleans.
-					'-bc',
-					// Compound boolean and string.
-					'-de',
-					'e',
-					// String true.
-					'-f',
-					'true',
-					// String false.
-					'-g',
-					'false',
-					// Equals string.
-					'-h=h',
-					// Equals string in double quotes.
-					'-i="i"',
-					// Equals string in escaped double quotes.
-					'-j="j"',
-					// Equals string in single quotes.
-					"-k='k'",
-					// String in double quotes.
-					'-l',
-					'"l"',
-					// String in single quotes.
-					'-m',
-					"'m'",
-					// String in escaped double quotes.
-					'-n',
-					'"n"',
-				],
-				options: {
-					a: 'a',
-					b: true,
-					c: true,
-					d: true,
-					e: 'e',
-					f: true,
-					g: false,
-					h: 'h',
-					i: 'i',
-					j: 'j',
-					k: 'k',
-					l: 'l',
-					m: 'm',
-					n: 'n',
-				},
-				positionalArguments: [],
-			},
-		],
-		[
-			/** @todo Make this its own test. */
-			'when there are options',
-			{
-				argv: [
-					'--a',
-					'--b=hello',
-					'--c',
-					'world',
-					'--d',
-					'false',
-					'--e=hi',
-					'--f="universe"',
-					"-g='sup'",
-				],
-				options: {
-					a: true,
-					b: 'hello',
-					c: 'world',
-					d: false,
-					e: 'hi',
-					f: 'universe',
-					g: 'sup',
-				},
-				positionalArguments: [],
-			},
-		],
-		[
-			'when there are all kinds of arguments',
-			{
-				argv: ['a', '-b', '-c', 'd', '-ef', '-gh', 'i', '--j', '--k', 'l', 'm'],
-				options: {
-					b: true,
-					c: 'd',
-					e: true,
-					f: true,
-					g: true,
-					h: 'i',
-					j: true,
-					k: 'l',
-				},
-				positionalArguments: ['a', 'm'],
-			},
-		],
-	])(
-		'%s',
-		(
-			_: string,
-			testCase: {
-				argv: string[]
-				options: Record<string, string | boolean>
-				positionalArguments: string[]
-			},
-		) => {
-			const processArgv = process.argv
-
-			beforeEach(() => {
-				process.argv = ['node', 'command', ...testCase.argv]
-			})
-
-			afterAll(() => {
-				process.argv = processArgv
-			})
-
-			it('should have correct options', () => {
-				expect(new CommandLine().options).toEqual(testCase.options)
-			})
-
-			it('should have correct positionalArguments', () => {
-				expect(new CommandLine().positionalArguments).toEqual(
-					testCase.positionalArguments,
-				)
-			})
+describe.each([
+	[
+		'when there are no command line arguments',
+		{ argv: [], options: {}, positionalArguments: [] },
+	],
+	[
+		'when there are positional arguments',
+		{
+			argv: ['hello', 'world'],
+			options: {},
+			positionalArguments: ['hello', 'world'],
 		},
-	)
-})
+	],
+	[
+		/** @todo Make this its own test. */
+		'when there are flags',
+		{
+			/* eslint-disable multiline-comment-style -- Ignores Prettier. */
+			/* eslint-disable no-useless-escape -- Tests escaped strings. */
+			argv: [
+				// String.
+				'-a',
+				'a',
+				// Compound booleans.
+				'-bc',
+				// Compound boolean and string.
+				'-de',
+				'e',
+				// String true.
+				'-f',
+				'true',
+				// String false.
+				'-g',
+				'false',
+				// Equals string.
+				'-h=h',
+				// Equals string in double quotes.
+				'-i="i"',
+				// Equals string in escaped double quotes.
+				// prettier-ignore
+				'-j=\"j\"',
+				// Equals string in single quotes.
+				"-k='k'",
+				// String in double quotes.
+				'-l',
+				'"l"',
+				// String in single quotes.
+				'-m',
+				"'m'",
+				// String in escaped double quotes.
+				'-n',
+				// prettier-ignore
+				'\"n\"',
+			],
+			/* eslint-enable multiline-comment-style */
+			/* eslint-enable no-useless-escape */
+			options: {
+				a: 'a',
+				b: true,
+				c: true,
+				d: true,
+				e: 'e',
+				f: true,
+				g: false,
+				h: 'h',
+				i: 'i',
+				j: 'j',
+				k: 'k',
+				l: 'l',
+				m: 'm',
+				n: 'n',
+			},
+			positionalArguments: [],
+		},
+	],
+	[
+		/** @todo Make this its own test. */
+		'when there are options',
+		{
+			argv: [
+				'--a',
+				'--b=hello',
+				'--c',
+				'world',
+				'--d',
+				'false',
+				'--e=hi',
+				'--f="universe"',
+				"-g='sup'",
+			],
+			options: {
+				a: true,
+				b: 'hello',
+				c: 'world',
+				d: false,
+				e: 'hi',
+				f: 'universe',
+				g: 'sup',
+			},
+			positionalArguments: [],
+		},
+	],
+	[
+		'when there are all kinds of arguments',
+		{
+			argv: ['a', '-b', '-c', 'd', '-ef', '-gh', 'i', '--j', '--k', 'l', 'm'],
+			options: {
+				b: true,
+				c: 'd',
+				e: true,
+				f: true,
+				g: true,
+				h: 'i',
+				j: true,
+				k: 'l',
+			},
+			positionalArguments: ['a', 'm'],
+		},
+	],
+])(
+	'%s',
+	(
+		_: string,
+		testCase: {
+			argv: string[]
+			options: Record<string, string | boolean>
+			positionalArguments: string[]
+		},
+	) => {
+		const processArgv = process.argv
 
-describe('CommandLine instance methods', () => {
+		beforeEach(() => {
+			process.argv = ['node', 'command', ...testCase.argv]
+		})
+
+		afterAll(() => {
+			process.argv = processArgv
+		})
+
+		it('should have correct options', () =>
+			expect(new CommandLine().options).toEqual(testCase.options))
+
+		it('should have correct positional arguments', () =>
+			expect(new CommandLine().positionalArguments).toEqual(
+				testCase.positionalArguments,
+			))
+	},
+)
+
+describe('when streams are bound', () => {
 	let streams: Streams
 
 	beforeEach(() => {

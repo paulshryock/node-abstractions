bd57a26b75ff6696083d292bf695ea05335867c4
Paul Shryock <paul@pshry.com>
2024-09-23 05:58:07 -0400

    Export CommandLine output options and streams

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-export type { CommandLine } from './CommandLine/CommandLine.ts'
+export type {
+	CommandLine,
+	CommandLineOutputOptions,
+} from './CommandLine/CommandLine.ts'
 export type { Exception } from './Exception/Exception.ts'
 export type { FileSystem } from './FileSystem/FileSystem.ts'
 export type { StackTrace } from './StackTrace/StackTrace.ts'
+export type { Streams as CommandLineStreams } from './CommandLine/Streams.ts'
 export { LocalCommandLine } from './CommandLine/LocalCommandLine.ts'
 export { LocalFileSystem } from './FileSystem/LocalFileSystem.ts'

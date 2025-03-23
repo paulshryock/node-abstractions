9852225d523b5c33ea978e480f88fbfb30f3865c
Paul Shryock <paul@pshry.com>
2025-03-22 20:16:42 -0400

    Allow compiled code to be 8 KiB

--- tests/acceptance/compile.test.ts
+++ tests/acceptance/compile.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from '@jest/globals'
 import { join } from 'node:path'
 import { stat } from 'node:fs/promises'
 
-const BUNDLE_MAXIMUM_KIB = 7
+const BUNDLE_MAXIMUM_KIB = 8
 const BUNDLE_MAXIMUM_SIZE = BUNDLE_MAXIMUM_KIB * 1024
 
 describe('compiled javascript bundle', () => {
